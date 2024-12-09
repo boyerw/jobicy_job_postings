6bf85d8c1dd5abbac3e12c97d3e03c3429fd5784
boyerw <wyatt.boyer@protonmail.com>
2024-12-09 14:41:52 -0500

    Create an EC2 instance on AWS

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+# terraform settings
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = ">= 4.16"
+        }
+    }
+    required_version = ">= 1.2.0"
+}
+
+# providers
+provider aws {}
+
+# resources
+resource "aws_instance" "webserver" {
+    # The first Linux AMI in the AMI catalog 
+    ami = "ami-0453ec754f44f9a4a"
+    instance_type = "t2.micro"
+    tags = {
+        name = "ExampleServer"
+    }
+}
+
+# input
+
+
+# output
+
